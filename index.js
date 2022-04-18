f6b5451aa953f3fce194eb8e0bc086dad10db2a4
Sachin-Sharma-1 <101580235+Sachin-Sharma-1@users.noreply.github.com>
2022-04-18 22:20:19 +0530

    Some minor changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const sun = document.querySelector(".sun");
 const body = document.querySelector("body");
-body.addEventListener("click", function(){
+sun.addEventListener("click", function(){
   body.classList.toggle("dark");
 });
